4414d7721f54a1f0133c87e10de6383fef15591d
Adriana <adrianacoradin44@gmail.com>
2024-08-29 22:34:53 -0300

    #000002

---
+++ Repositorios/UsuarioRepositorio.cs
@@ -0,0 +1,45 @@
+﻿using WebAPIs.Data;
+using WebAPIs.Models;
+using WebAPIs.Repositorios.Interfaces;
+
+namespace WebAPIs.Repositorios
+{
+    public class UsuarioRepositorio : IUsuarioRepositorio
+    {
+        private readonly SistemaTaferasDBContext _dbContext;
+        public UsuarioRepositorio(SistemaTaferasDBContext sistemaTaferasDBContext) 
+        {
+            _dbContext = sistemaTaferasDBContext;
+
+
+        }
+
+
+        public Task<UsuarioModel> BuscarPorId(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<UsuarioModel>> BuscarTodosUsuarios()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<UsuarioModel> Adicionar(UsuarioModel usuario)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> Apagar(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<UsuarioModel> Atualizar(UsuarioModel usuario, int id)
+        {
+            throw new NotImplementedException();
+        }
+
+
+    }
+}
